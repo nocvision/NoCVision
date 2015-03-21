6f50f6fece578caced11e2c403c8f8c3e9fced2b
Vaibhav <vgogte.umich.edu>
2015-03-20 21:04:44 -0400

    event mode changes

--- src/nocvision_config.py
+++ src/nocvision_config.py
@@ -1,27 +1,3 @@
-# Copyright (c) 2015, University of Michigan
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-#
-# Redistributions of source code must retain the above copyright notice, this 
-# list of conditions and the following disclaimer.
-# Redistributions in binary form must reproduce the above copyright notice, this
-# list of conditions and the following disclaimer in the documentation and/or
-# other materials provided with the distribution.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
-# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
-# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
-# ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-
 from __future__ import division
 from matplotlib.pylab import *
 from matplotlib.colors import LinearSegmentedColormap
@@ -208,7 +184,7 @@ for section in config.sections():
       continue
 
 
-eventParsePattern = re.compile(r'^(motivator):(\D*):')
+eventParsePattern = re.compile(r'^(nocvision):(\D*):')
 clockPattern = re.compile(r'(clock_)(\d*)')
 pidPattern = re.compile(r'(pid_)(\d*)')
 nodePattern = re.compile(r'(node_)(\d*)')
@@ -233,7 +209,7 @@ with open(conf_file_name,'r') as event_file:
          "vc": eval(vcPattern.search(line).groups()[1])
 	 }
          event_dict.append(event_info) 
-         if eventParsePattern.search(line).groups()[1] == 'trans':
+         if eventParsePattern.search(line).groups()[1] == 'trace':
 	   event_packet_ids.append(pidPattern.search(line).groups()[1])
 	 if ((eventParsePattern.search(line).groups()[1] == 'pin') | (eventParsePattern.search(line).groups()[1] == 'unpin')):
 	   event_packet_ids.append("-1")
